5a4819e9f17b2f6f08b470526459b02d1c59f329
Tomas Agustin Ramirez <tramirez@MacBook-Pro-Tomas.local>
2022-10-10 23:56:34 -0300

    reordeno y agrego el rollback

--- etc/src/migracion_001.sql
+++ etc/src/migracion_001.sql
@@ -1,9 +1,18 @@
+CREATE DATABASE gdd
+go
+
+USE gdd
+go
+
+-- Table creation
+
 CREATE TABLE Compra_Producto (
     compra_id int NOT NULL FOREIGN KEY REFERENCES Compra(compra_id),
     producto_variante_id int NOT NULL FOREIGN KEY REFERENCES Producto_Variante(producto_variante_id),
     compra_producto_precio decimal(18,2) NOT NULL,
     compra_producto_cantidad decimal(18,0) NOT NULL
-);
+)
+go
 
 CREATE TABLE Producto_variante (
     producto_variante_id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
@@ -12,14 +21,16 @@ CREATE TABLE Producto_variante (
     producto_variante_codigo nvarchar(50) NOT NULL,
     producto_variante_precio decimal(18,2) NOT NULL,
     producto_variante_cantidad decimal(18,0) NOT NULL
-);
+)
+go
 
 CREATE TABLE Venta_Producto (
     venta_id int NOT NULL FOREIGN KEY REFERENCES Venta(venta_id),
     producto_variante_id int NOT NULL FOREIGN KEY REFERENCES Producto_variante(producto_variante_id),
     venta_producto_precio decimal(18,2) NOT NULL, 
     venta_producto_cantidad decimal(18,0) NOT NULL
-);
+)
+go
 
 CREATE TABLE Producto (
     producto_id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
@@ -29,30 +40,49 @@ CREATE TABLE Producto (
     producto_codigo nvarchar(50) NOT NULL,
     producto_nombre nvarchar(50) NOT NULL,
     producto_descripcion nvarchar(50) NOT NULL
-);
+)
+go
 
 CREATE TABLE Variante (
     variante_id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
     tipo_variante_id int NOT NULL FOREIGN KEY REFERENCES Tipo_Variante(tipo_variante_id),
     variante_descripcion nvarchar(50) NOT NULL
-);
+)
+go
 
 CREATE TABLE Tipo_Variante (
     tipo_variante_id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
     tipo_variante_descripcion nvarchar(50) NOT NULL
-);
+)
+go
 
 CREATE TABLE Material (
     material_id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
     material_descripcion nvarchar(50) NOT NULL
-);
+)
+go
 
 CREATE TABLE Marca (
     marca_id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
     marca_descripcion nvarchar(255) NOT NULL
-);
+)
+go
 
 CREATE TABLE Categoria (
     categoria_id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
     categoria_descripcion nvarchar(255) NOT NULL
-);
+)
+go
+
+-- Index creation
+
+-- CREATE INDEX itipo_variante_id
+--     on dbo.Tipo_Variante (tipo_variante_id)
+-- go    
+
+
+-- Data migration: Aca colocar el nombre de base de datos que usaste
+
+USE dbo_gestion;
+
+-- TODO: Agregar la migracion de data.
