0a7a57e568bace6cbefc651abd0f95ec4457782b
EstebanQ99 <javiquintana99@hotmail.com>
2022-10-29 17:02:06 -0300

    rollback

--- etc/src/rollback.sql
+++ etc/src/rollback.sql
@@ -1,6 +1,24 @@
 USE [GD2C2022]
 GO
 
+drop table nn.Venta_Canal
+drop table nn.Venta_Medio_Envio
+drop table nn.Venta_Medio_Pago
+drop table nn.Cliente
+drop table nn.Cliente_Direccion
+drop table nn.Localidad
+drop table nn.Provincia
+drop table nn.Codigo_Postal
+
+drop procedure nn.Insert_Venta_Canal
+drop procedure nn.Insert_Venta_Medio_Envio
+drop procedure nn.Insert_Venta_Medio_Pago
+drop procedure nn.Insert_Cliente
+drop procedure nn.Insert_Cliente_Direccion
+drop procedure nn.Insert_Localidad
+drop procedure nn.Insert_Provincia
+drop procedure nn.Insert_Codigo_Postal
+
 drop table [gd_esquema].[Material]
 GO
 
